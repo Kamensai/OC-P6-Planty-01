ee2abeaa6e98950c5505ecce4eb1142647da53f7
Khamvongsa Saisana <khamvongsa.v@hotmail.fr>
2026-01-07 10:27:22 +0100

    first commit after debug crash on port

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+
+</main><!-- #main -->
+	<footer id="site-footer" class="header-footer-group">
+		<p>	Mentions légales </p>
+	</footer><!-- #site-footer -->
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
