a1c3c42c36b74746b61bb1710d0087b0c30ab3a0
Khamvongsa Saisana <khamvongsa.v@hotmail.fr>
2026-01-12 17:15:53 +0100

    Link Admin added to the menu.

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -14,4 +14,14 @@ function theme_enqueue_styles(){
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
 }
 
+
+function add_extra_item_to_nav_menu( $items, $args ) {
+	if (is_user_logged_in() && $args->menu === 'main-menu') {
+		$new_item = '<li class="menu_item_user_is_logged"><a href="/planty">Admin</a></li>';
+		$items = preg_replace('/<\/li>/', '</li>' . $new_item, $items, 1);
+	}
+	return $items;
+}
+
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
 ?>
